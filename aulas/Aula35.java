379d66ca0ce0f575dd98283ab1dd0ada6b30755c
camilaperon <csperontads@gmail.com>
2024-11-29 22:20:21 -0300

    commit 29/11

---
+++ aulas/Aula35.java
@@ -0,0 +1,16 @@
+package aulas;
+
+public class Aula35 {
+    
+
+    //ESTUDAR COUTING SORT
+
+
+
+
+    public static void ex01(){
+        int meio;
+        int[] v0 = {7, 2, 9, 2, 7, 1, 9, 2, 0};
+        //int[] v0_ordenado = 
+    }
+}
